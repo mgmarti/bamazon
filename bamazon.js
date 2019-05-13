4b284f22c3543783e01199beb8dba4b7bd9d8f0b
monica <mogimartinez@gmail.com>
2019-05-13 18:00:27 -0500

    added item id prompt

--- bamazon.js
+++ bamazon.js
@@ -1,24 +1,75 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+require('console.table')
 
+// MySQL connection
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.SQL_KEY,
+    password: "root",
     database: "products_db"
 });
 
-connection.connect(function(err){
-    // console.log("Connected as id: "+connection.threadID);
+connection.connect(function (err) {
+    // console.log("Connected as id: " + connection.threadId);
+    displayInventory();
 });
 
 
+//Displays Inventory
+function displayInventory() {
+    connection.query("SELECT * FROM products_db.products", function (error, response) {
+        if (error) throw error
+        console.table(response)
+        promptCustomer();
+    })
 
-// inquirer
-//   .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   });
+}
+
+
+
+function promptCustomer() {
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: 'input',
+                name: 'item_id',
+                message: "Select the item you would like to purchase by item id.",
+            },
+            {
+                type: 'input',
+                name: 'quantity',
+                message: "How many units?",
+            }
+        ])
+        .then(function (val) {
+            let productId = val.choice;
+            console.log("You chose: " + productId)
+            checkInventory(productId);
+            promptQuantity();
+
+        });
+}
+
+// function promptQuantity() {
+//     inquirer
+//         .prompt([{
+//                 type: 'input',
+//                 name: 'quantity',
+//                 message: "How many units?",
+//             }
+
+//         ])
+//         .then(function (val) {
+
+//             var productQuantity = val.quantity;
+//             console.log(productQuantity)
+
+//         });
+// }
+
+function checkInventory(productId) {
+    console.log("Checking for: " + productId)
+}
