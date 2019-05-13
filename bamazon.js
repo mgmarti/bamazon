95c31e7e57d37e432cd6503e488b8fe3bb2aa62c
monica <mogimartinez@gmail.com>
2019-05-13 12:12:27 -0500

    installed npm mysql

---
+++ bamazon.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
